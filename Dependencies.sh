5057d4872a20d4a610122f005d18dd9df6ca44a7
louis <louis@northe-online.de>
2023-01-14 16:31:15 +0100

    Added fifo for spectrum

--- Dependencies.sh
+++ Dependencies.sh
@@ -2,4 +2,4 @@
 
 echo "Checking and installing dependencies for the T-UGS"
 sudo apt-get install portaudio19-dev libatlas-base-dev python3-scipy -y 
-pip install pyaudio pylabs matplotlib seaborn smbus mpu6050-raspberrypi librosa 
+pip install pyaudio pylabs matplotlib seaborn smbus mpu6050-raspberrypi 
--- TUGS.py
+++ TUGS.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import os
 import pyaudio
 import numpy as np
 import pylab
@@ -7,20 +8,21 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.fft import fftshift
 import scipy.fftpack
-import librosa
+from scipy.ndimage import shift
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 2048
+FIFO_WINDOW = 128
+
+FIFO = np.zeros((CHUNK, CHUNK*FIFO_WINDOW), dtype=np.int16)
 
 i=0
-f,ax = plt.subplots(3)
+f,ax = plt.subplots(2)
 
 x = np.arange(10000)
 y = np.random.randn(10000)
-
-#plt.grid()
 
 li, = ax[0].plot(x, y)
 ax[0].set_xlim(0,1000)
@@ -28,14 +30,9 @@ ax[0].set_ylim(-5000,5000)
 ax[0].set_title("Raw Audio Signal", loc='center', wrap=True)
 
 li2, = ax[1].plot(x, y)
-ax[1].set_xlim(0,5000)
+ax[1].set_xlim(0, 2e3)
 ax[1].set_ylim(0,100)
 ax[1].set_title("Fast Fourier Transform", loc='center', wrap=True)
-
-ax[2].set_title("Spectrogram", loc='center', wrap=True)
-ax[2].set_xlabel('Time (s)')
-ax[2].set_ylabel('Frequencies (Hz)')
-
 
 plt.pause(0.001)
 plt.tight_layout()
@@ -52,37 +49,42 @@ def selectMic():
     print("Select microphone by id: ")
     return int(input())
 
-def bpmToMS(bpm):
-    return 60e3/bpm
-
 def plot_data(in_data):
     audio_data = np.frombuffer(in_data, dtype=np.int16)
 
-    # A butterworth bandpass  filter for filtering the high noised hissing from the cheap USB mic and the low end garbage
-    sos = signal.butter(10, [10, 800], 'bp', fs=RATE, output='sos')
-    audio_data = signal.sosfilt(sos, audio_data)
+    # Butterworth bandpass  filter for filtering the high noised hissing from the cheap USB mic and the low end garbage
+    # Butterworth bandstop to focus on the interesting frequencies for the step detection (~40Hz and ~600-700HZ)
+    sos_bp = signal.butter(10, [10, 900], 'bp', fs=RATE, output='sos')
+    sos_bs = signal.butter(10, [50, 600], 'bs', fs=RATE, output='sos')
+    audio_data_bp = signal.sosfilt(sos_bp, audio_data)
+    audio_data_bs = signal.sosfilt(sos_bs, audio_data_bp)
     
     # Blackman window function as it has the wide main lobe and surpresses more the side lobes 
-    audio_data_window = audio_data * np.blackman(len(audio_data))
+    audio_data_window = audio_data_bs * np.blackman(len(audio_data_bs))
+
+    # FFT in dB drom the windowed audio signal, using all cores of the host
     dfft = 20* np.log10(np.abs(scipy.fftpack.rfft(audio_data_window)))
     
-    # To be tested: mode and window
-    freq, times, spectrogram = signal.spectrogram(audio_data, RATE, window='blackman')
-    spec_mesh = ax[2].pcolormesh(times, freq, 10.*np.log10(spectrogram), shading='gouraud')
-        
+    # Adding values to the FIFO 
+    FIFO[:, 0] = dfft
+    # Shifting the FFTs along the second dimension
+    FIFO = shift(FIFO, (0, 1), cval=np.NaN)
+    
+    # Spectrogram from the windowed audio
+    #freq, times, spectrogram = signal.spectrogram(audio_data, RATE, window='blackman')
+
+    # Subplot with spectrogram
+    #spec_mesh = ax[2].pcolormesh(times, freq, 10.*np.log10(spectrogram), shading='gouraud')
+
     # Bar for seeing the db color relation
     #bar = plt.colorbar(spec_mesh, ax=ax[2])
     #bar.set_label('Amplitude (dB)')
 
-    li.set_xdata(np.arange(len(audio_data)))
+    x_axis_fft = np.arange(len(audio_data))
+    li.set_xdata(x_axis_fft)
     li.set_ydata(audio_data)
     li2.set_xdata(np.arange(len(dfft))*10.)
     li2.set_ydata(dfft)
-    
-    # Experimental
-    # Try to detect steps by extracting the bps and if it fits into the step range we will take it as a step
-    tempo, beats = librosa.beat.beat_track(y=audio_data, sr=RATE)   
-    print("Your steps dance with a BPM of " % bpmToMS(tempo))
     
     print("--- %s seconds ----" % (time.time() - time_t1))
     
