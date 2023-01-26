8eccbf665a1d7c0497dd92850370e690609d43b6
NortheLo <louis@northe-online.de>
2023-01-26 12:58:21 +0100

    Added callback, tweaked threshold, inreased window size

--- Dependencies.sh
+++ Dependencies.sh
@@ -5,3 +5,4 @@ sudo apt-get install portaudio19-dev libatlas-base-dev python3-scipy -y
 pip install pyaudio pylabs matplotlib seaborn smbus mpu6050-raspberrypi
 
 #It is very important to install pyadio versio >=0.2.12 as older versions are not working correctly with newer python versions
+#Check with `pip show pyaudio`
